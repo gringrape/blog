b50d1d5a529a5bf888f839dd902e77651a4d9616
gringrape <gringrape200@gmail.com>
2024-01-01 12:44:40 +0900

    본문 스타일

--- src/pages/{markdownRemark.frontmatter__slug}.tsx
+++ src/pages/{markdownRemark.frontmatter__slug}.tsx
@@ -1,4 +1,45 @@
+import './markdown-style.css';
+
+import styled from 'styled-components';
+
 import { graphql } from 'gatsby';
+
+import hljs from 'highlight.js';
+
+import 'highlight.js/styles/github-dark.css';
+import { useEffect } from 'react';
+
+const Article = styled.article`
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  margin-bottom: 5em;
+  padding: 45px;
+
+  @media (max-width: 767px) {
+    padding: 15px;
+  }
+`;
+
+const Header = styled.header`
+  margin-bottom: 5em;
+
+  h1 {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border-bottom: none;
+    
+    strong {
+      font-size: 1.5em;
+    }
+  
+    span {
+      font-size: 0.5em;
+    }
+  }
+`;
 
 export default function BlogPostTemplate({
   data,
@@ -15,16 +56,21 @@ export default function BlogPostTemplate({
 }) {
   const { markdownRemark } = data;
   const { frontmatter, html } = markdownRemark;
+
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
+
   return (
-    <div>
-      <div>
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+    <Article className="markdown-body">
+      <Header>
+        <h1>
+          <strong>{frontmatter.title}</strong>
+          <span>{frontmatter.date}</span>
+        </h1>
+      </Header>
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+    </Article>
   );
 }
 
